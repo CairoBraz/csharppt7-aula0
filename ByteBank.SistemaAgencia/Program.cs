676303641035a9c3fbc4bec181ca58df0e163385
CairoBraz <kairobc@hotmail.com>
2022-06-08 08:39:18 -0300

    Criando a classe de lista genérica Lista<T>. E criando uma lista de int no program.

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,21 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            Lista<int> idades = new Lista<int>();
+
+            idades.Adicionar(5);
+            idades.Adicionar(10);
+            idades.AdicionarVarios(3, 4, 10, 23);
+            
+            Console.WriteLine(SomarVarios(1, 2, 4, 5, 6));
+            Console.WriteLine(SomarVarios(55, 10, 20, 99, 100));
+
+            Console.ReadLine();
+
+        }
+
+        static void TestaListaDeObject()
+        {
             ListaDeObject listaDeIdades = new ListaDeObject();
 
             listaDeIdades.Adicionar(5);
@@ -25,11 +40,6 @@ namespace ByteBank.SistemaAgencia
                 int idade = (int)listaDeIdades[i];
                 Console.WriteLine($"Idade na posição {i}: {idade}");
             }
-
-            Console.WriteLine(SomarVarios(1, 2, 4, 5, 6));
-            Console.WriteLine(SomarVarios(55, 10, 20, 99, 100));
-
-            Console.ReadLine();
 
         }
 
