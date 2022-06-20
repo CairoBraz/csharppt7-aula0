35524562237b9c1b00a18e68e4b4fe6f3b74fd68
CairoBraz <kairobc@hotmail.com>
2022-06-20 09:05:09 -0300

    Simplificando o codigo trazendo chamando o where() junto com o OrderBy().

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -30,8 +30,9 @@ namespace ByteBank.SistemaAgencia
             //contas.Sort(new ComparadorContaCorrentePorAgencia());
 
             // var listaSemNulos = new List<ContaCorrente>();
-            IEnumerable<ContaCorrente> contasNaoNulas = contas.Where(conta => conta != null);
-            IOrderedEnumerable<ContaCorrente> contasOrdenadas = contasNaoNulas.OrderBy(conta => conta.Numero);
+            var contasOrdenadas = contas
+                .Where(conta => conta != null)
+                .OrderBy(conta => conta.Numero);
 
             foreach (var conta in contasOrdenadas)
             {
