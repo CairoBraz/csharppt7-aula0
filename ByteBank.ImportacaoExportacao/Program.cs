d31d21e52fda926560edd5df939bf2f92b505146
CairoBraz <kairobc@hotmail.com>
2022-06-23 09:16:51 -0300

    Criando o projeto ImportacaoExportacao na Solution e adicionando a referência ao Projeto ByteBank.SistemaAgencia

---
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.SistemaAgencia;
+
+namespace ByteBank.ImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(123, 45678);
+        }
+    }
+}
