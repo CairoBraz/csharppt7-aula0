7455fd51acea6a53715c63edf0b97dc5f7a8a96b
CairoBraz <kairobc@hotmail.com>
2022-06-29 08:59:24 -0300

    Utilizando a classe StreamReader para ler a variável fluxoDeArquivo que salva o FileStream. Para isso evocamos o metodo ReadLine. O StreamReader implementa IDispose, entao colocamos dentro de um bloco using para que o metodo dispose seja evocado ao final. Adequamos o codigo pois teremos dois using em sequencia.

--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -13,8 +13,20 @@ namespace ByteBank.ImportacaoExportacao
     {
         static void Main(string[] args)
         {
-            LidandoComFileStreamDiretamente();
-            
+            var enderecoDoArquivo = "contas.txt";           //o arquivo esta na pasta Bin/Debug do projeto ByteBank.ImportacaoExportacao e se chama apenas contas
+
+            using(var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    //var linha = leitor.Read();              //Ler apenas o primeiro byte. O retorno é um int
+                    //var linha = leitor.ReadToEnd();         // Ler todo o arquivo, montar uma string única e salvar na variável linha. Usar com cuidado.
+                    var linha = leitor.ReadLine();            //Ler a linha do arquivo
+                    Console.WriteLine(linha);
+                }
+             }
+
             Console.ReadLine();
 
         }
