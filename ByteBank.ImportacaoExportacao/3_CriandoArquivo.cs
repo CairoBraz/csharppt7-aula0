19ecb8e8631eaefe752cc0ab08400a2707ed64b9
CairoBraz <kairobc@hotmail.com>
2022-07-25 08:24:45 -0300

    Criando um FileStream com FileMode.Create para criar um arquivo e salvar os registros.

--- ByteBank.ImportacaoExportacao/3_CriandoArquivo.cs
+++ ByteBank.ImportacaoExportacao/3_CriandoArquivo.cs
@@ -18,6 +18,10 @@ namespace ByteBank.ImportacaoExportacao
             using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
             {
                 var contaComoString = "456,7895,4785.40,Gustavo Santos";
+                var encoding = Encoding.UTF8;
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
             }
         }
 
--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -13,6 +13,7 @@ namespace ByteBank.ImportacaoExportacao
     {
         static void Main(string[] args)
         {
+            CriarArquivo();
             Console.ReadLine();
         }
     }
