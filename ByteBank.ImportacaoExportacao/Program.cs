cfc0f1cab9323b3c0845f59974d12b72bc00796f
CairoBraz <kairobc@hotmail.com>
2022-07-27 23:08:11 -0300

    Usando os Streams Binarios

--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -15,7 +15,20 @@ namespace ByteBank.ImportacaoExportacao
         {
             //CriarArquivo();
             //CriarArquivoComWriter();
-            TestaEscrita();
+            //TestaEscrita();
+
+            //using(var fs = new FileStream("testaTipos.txt", FileMode.Create))
+            //using( var escritor = new StreamWriter(fs))
+            //{
+            //    escritor.WriteLine(true);
+            //    escritor.WriteLine(false);
+            //    escritor.WriteLine(123654849);
+            //    escritor.WriteLine(522.45);
+            //    escritor.WriteLine("Pedro Julio");
+            //}
+
+            //EscritaBinaria();
+            LeituraBinaria();
 
             Console.WriteLine("Aplicação finalizada...");
             Console.ReadLine();
